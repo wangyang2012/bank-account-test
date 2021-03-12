5c24cb297b025b975bedd65fe657eb42ee26adaa
Yang <wangyang1712@gmail.com>
2021-03-12 11:53:14 +0100

    [US03] Check operations: implementations

--- src/main/java/com/bank/service/CustomerServiceImpl.java
+++ src/main/java/com/bank/service/CustomerServiceImpl.java
@@ -1,15 +1,19 @@
 package com.bank.service;
 
 import com.bank.model.Customer;
+import com.bank.model.Operation;
 import com.bank.model.exception.BankException;
 import com.bank.model.exception.TechnicalException;
 import com.bank.utils.IdUtil;
 import org.apache.commons.lang3.StringUtils;
 import java.math.BigDecimal;
+import java.util.logging.Logger;
 
 public class CustomerServiceImpl implements ICustomerService {
 
 	private IAccountService accountService = new AccountServiceImpl();
+	private IOperationService operationService = new OperationServiceImpl();
+	private static final Logger LOGGER = Logger.getLogger(CustomerServiceImpl.class.getName());
 
 	@Override
 	public Customer createCustomer(String name) throws BankException {
@@ -21,6 +25,18 @@ public class CustomerServiceImpl implements ICustomerService {
 
 	@Override
 	public void printCustomerBalance(Customer customer) throws BankException {
+		if (customer == null) {
+			throw new TechnicalException("Customer cannot be null");
+		}
 
+		StringBuilder sb = new StringBuilder();
+		sb.append("******* BANK ACCOUNT INFORMATION ******\n");
+		sb.append("Customer: " + customer.getName() + "\n");
+		sb.append("Account number: " + customer.getAccount().getId() + "\n");
+		for (Operation operation : customer.getAccount().getOperations()) {
+			sb.append(this.operationService.printOperationToString(operation) + "\n");
+		}
+		sb.append("Balance: " + customer.getAccount().getBalance().setScale(2) + "\n");
+		LOGGER.info(sb.toString());
 	}
 }
--- src/main/java/com/bank/service/OperationServiceImpl.java
+++ src/main/java/com/bank/service/OperationServiceImpl.java
@@ -9,6 +9,7 @@ import com.bank.model.exception.TechnicalException;
 import com.bank.utils.IdUtil;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class OperationServiceImpl implements IOperationService {
 
@@ -27,7 +28,19 @@ public class OperationServiceImpl implements IOperationService {
 
 	@Override
 	public String printOperationToString(Operation operation) throws BankException {
-		return null;
+		if (operation == null) {
+			throw new TechnicalException("Operation cannot be null");
+		}
+		StringBuilder sb = new StringBuilder();
+		sb.append(operation.getDateTime().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+		sb.append("\t\t");
+		sb.append(operation.getAction());
+		sb.append("\t");
+		if (OperationActionEnum.DEPOSIT.equals(operation.getAction())) {
+			sb.append("\t");
+		}
+		sb.append(operation.getAmount().setScale(2));
+		return sb.toString();
 	}
 
 }
