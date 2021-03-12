b2c4e2812f88e88109dae57c925c587a7e409288
Yang <wangyang1712@gmail.com>
2021-03-12 11:10:21 +0100

    init project
    change pom

---
+++ src/main/java/com.bank/App.java
@@ -0,0 +1,9 @@
+package com.bank;
+
+public class App
+{
+    public static void main( String[] args ) throws Exception {
+        ApplicationDemo demo = new ApplicationDemo();
+        demo.demo();
+    }
+}
---
+++ src/main/java/com.bank/ApplicationDemo.java
@@ -0,0 +1,8 @@
+package com.bank;
+
+public class ApplicationDemo {
+
+    public void demo() throws Exception {
+        System.out.println("It works");
+    }
+}
